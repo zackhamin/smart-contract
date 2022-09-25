d3542a4bfb615c4725ac421554d49400596af1f3
zackhamin <ishaqhamin@gmail.com>
2022-09-25 15:29:06 +0100

    add folder structure and base test for inital contract

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,2 +1,12 @@
-pragma solidity ^0.8.0
+pragma solidity 0.8.17;
 
+contract HelloWorld {
+
+function hello() public pure returns (string memory) {
+    return "hello, world";
+}
+
+
+
+
+}
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,4 +1,7 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
 module.exports = {
   solidity: "0.8.17",
 };
