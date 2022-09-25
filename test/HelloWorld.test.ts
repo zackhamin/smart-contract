8fa63db2a57f1c3f0af540b496a4e875a463325e
zackhamin <ishaqhamin@gmail.com>
2022-09-25 15:37:10 +0100

    firs test unit added

--- test/HelloWorld.test.ts
+++ test/HelloWorld.test.ts
@@ -1,1 +1,18 @@
 import "@nomiclabs/hardhat-ethers";
+import { ethers } from "hardhat";
+import { expect } from "chai";
+
+describe("hello world", () => {
+  it("should say hi", async () => {
+    //set up
+    //deploy
+    //call our function to test
+
+    //deploy
+    const HelloWorld = await ethers.getContractFactory("HelloWorld");
+    const hello = await HelloWorld.deploy();
+    await hello.deployed();
+
+    expect(await hello.hello()).to.equal("hello, world");
+  });
+});
